98b8ebd84df3dd9b5ea6792a455a9583153562b3
Oleg Grenrus <oleg.grenrus@iki.fi>
2018-03-21 09:16:49 +0200

    Add add-bound command

--- src/Main.hs
+++ src/Main.hs
@@ -33,7 +33,11 @@ import           Data.Time.Clock.POSIX                  (getPOSIXTime)
 import qualified Distribution.Package                   as C
 import qualified Distribution.PackageDescription        as C
 import qualified Distribution.PackageDescription.Parsec as C
+import qualified Distribution.Parsec.Class              as C
 import qualified Distribution.Parsec.Common             as C
+import qualified Distribution.Parsec.Field              as C
+import qualified Distribution.Parsec.Parser             as C
+import qualified Distribution.Pretty                    as C
 import qualified Distribution.Verbosity                 as C
 import qualified Distribution.Version                   as C
 import qualified Distribution.Text                      as C
@@ -463,6 +467,16 @@ nsplitAt n = splitAt i
   where
     i = fromMaybe (error "nsplitAt: overflow") $ toIntegralSized n
 
+findLibrarySection :: [C.Field C.Position] -> Maybe (Int, Int)
+findLibrarySection [] = Nothing
+findLibrarySection (C.Section (C.Name (C.Position row _) "library") [] fs : _) =
+    Just (row, findIndent fs)
+  where
+    findIndent [] = 4
+    findIndent (f : _) = case C.fieldAnn f of
+        C.Position _ col -> pred col
+findLibrarySection (_ : fs) = findLibrarySection fs
+
 ----------------------------------------------------------------------------
 -- CLI Interface
 
@@ -505,6 +519,13 @@ data CheckROptions = CheckROptions
   , optCROrig :: FilePath
   } deriving Show
 
+data AddBoundOptions = AddBoundOptions
+  { optABPackageName  :: C.PackageName
+  , optABVersionRange :: C.VersionRange
+  , optABMessage      :: [String]
+  , optABFiles        :: [FilePath]
+  } deriving Show
+
 data Command
     = ListCabal !ListCOptions
     | PullCabal !PullCOptions
@@ -513,6 +534,7 @@ data Command
     | PushCandidate !PushPCOptions
     | CheckRevision !CheckROptions
     | IndexShaSum   !IndexShaSumOptions
+    | AddBound !AddBoundOptions
     deriving Show
 
 optionsParserInfo :: ParserInfo Options
@@ -529,6 +551,9 @@ optionsParserInfo
 
   where
     bstr = BS8.pack <$> str
+
+    prsc :: C.Parsec s => OA.ReadM s
+    prsc = OA.eitherReader C.eitherParsec
 
     vrange = do
         s <- str
@@ -566,6 +591,11 @@ optionsParserInfo
                                                         <*> OA.argument str (metavar "INDEX-TAR")
                                                         <*> optional (OA.argument str (metavar "BASEDIR")))
 
+    addboundParser = AddBound <$> (AddBoundOptions <$> OA.argument prsc (metavar "DEPENDENCY")
+                                                   <*> OA.argument prsc (metavar "VERSIONRANGE")
+                                                   <*> many (OA.option str (OA.short 'm' <> OA.long "message" <> metavar "MSG" <> help "Use given MSG as a comment. If multiple -m options are given, their values are concatenated with 'unlines'."))
+                                                   <*> some (OA.argument str (metavar "CABALFILES...")))
+
     oParser
         = Options <$> switch (long "verbose" <> help "enable verbose output")
                   <*> option bstr (long "hostname"  <> metavar "HOSTNAME" <> value "hackage.haskell.org"
@@ -584,6 +614,8 @@ optionsParserInfo
                                                    (progDesc "validate revision"))
                                          , command "index-sha256sum" (info (helper <*> indexssParser)
                                                    (progDesc "generate sha256sum-format file"))
+                                         , command "add-bound" (info (helper <*> addboundParser)
+                                                   (progDesc "add bound to the library section of a package. .cabal file is edited in place"))
                                          ])
 
     verOption = infoOption verMsg (long "version" <> help "output version information and exit")
@@ -769,6 +801,30 @@ mainWithOptions Options {..} = do
        IndexShaSum opts -> IndexShaSum.run opts
 
 
+       AddBound AddBoundOptions {..} -> forM_ optABFiles $ \fp -> do
+           old <- BS.readFile fp
+
+           -- idea is simple:
+           -- - .cabal is line oriented file
+           -- - find "library" section start
+           -- - bonus: look of an indentation used from the next field/section there
+           -- - insert data into a bytestring "manually"
+           fs <- either (exitFailureWith . show) return $ C.readFields old
+           (lin, indent) <- maybe
+               (exitFailureWith $ "Cannot find library section in " ++ fp)
+               return
+               (findLibrarySection fs)
+
+           let msgLines  = map ("-- " ++) optABMessage
+               bdLine    = "build-depends: " ++ C.prettyShow optABPackageName ++ " " ++ C.prettyShow optABVersionRange
+               midLines  = [ BS8.pack $ replicate indent ' ' ++ l
+                           | l <- msgLines ++ [bdLine]
+                           ] ++ [""] -- also add an empty line separator
+               (preLines, postLines) = splitAt lin $ BS8.lines old
+               new = BS8.unlines (preLines ++ midLines ++ postLines)
+
+           BS.writeFile fp new
+
    return ()
   where
     mkHConn = do
@@ -802,6 +858,10 @@ mainWithOptions Options {..} = do
       where
         pdesc0 = parseGenericPackageDescription' cabdata0
         (_,_,xrev0) = pkgDescToPkgIdXrev pdesc0
+
+    exitFailureWith e = do
+        putStrLn e
+        exitFailure
 
     parseGenericPackageDescription' bs =
         case snd $ C.runParseResult $ C.parseGenericPackageDescription bs of
